a3825a91d331f8c11f3374e75c3130201443b17c
Richard Howarth <r.howarth@airmaxgroup.com>
2014-11-06 15:00:48 +0000

    Remove unused references Add/modify comments Add option to send preformatted json payload Add option to specify whether to look for https or not when checking for valid uri

--- SlackNotify/SlackNotify/Notify.cs
+++ SlackNotify/SlackNotify/Notify.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Collections.Specialized;
 using System.IO;
 using System.Net;
-//using System.Net.Http;
-//using System.Runtime.Serialization.Json;
 using System.Text;
-//using System.Threading.Tasks;
 
 using Newtonsoft.Json;
 
@@ -20,6 +16,8 @@ namespace SlackNotify
         /// <summary>
         /// Initialises an instance of the Notify class. Requires that a json config file exists containing the webhook URL.
         /// </summary>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="UriFormatException"></exception>
         public Notify()
         {
             FileInfo fileInfo = new FileInfo(CONFIG_FILE);
@@ -30,7 +28,7 @@ namespace SlackNotify
             string configString = reader.ReadToEnd();
             config = JsonConvert.DeserializeObject<Config>(configString);
 
-            if (IsValidUri(config.WebHookURL) == false) throw new UriFormatException("Invalid WebHookURL");
+            if (IsValidUri(config.WebHookURL, true) == false) throw new UriFormatException("Invalid WebHookURL");
             
         }
 
@@ -39,6 +37,8 @@ namespace SlackNotify
         /// Initialises an instance of the Notify class. Doesn't require a json config file to exist.
         /// </summary>
         /// <param name="WebHookURL">URL provided by SlackHQ when configuring an 'Incoming WebHook' integration</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="UriFormatException"></exception>
         public Notify(string WebHookURL){
 
             if (string.IsNullOrEmpty(WebHookURL)) throw new ArgumentException("Argument is null or empty");
@@ -46,7 +46,7 @@ namespace SlackNotify
 
             config.WebHookURL = WebHookURL;
 
-            if (IsValidUri(config.WebHookURL) == false) throw new UriFormatException("Invalid WebHookURL");
+            if (IsValidUri(config.WebHookURL, true) == false) throw new UriFormatException("Invalid WebHookURL");
         }
 
 
@@ -61,35 +61,52 @@ namespace SlackNotify
         }
 
 
+        /// <summary>
+        /// send the message to slack, to the specified channel
+        /// </summary>
+        /// <param name="channel">the channel to send the message to. if blank the message will be posted to the channel specified in the slack integration screen</param>
+        /// <param name="message">the message to send</param>
+        /// <returns>true if successful; otherwise false</returns>
         public bool Send(string channel, string message)
         {
             return Send(channel, message, "", "");
         }
 
 
-        public bool Send(string channel, string message, string user)
+        /// <summary>
+        /// sends the message to slack, to the specified channel, as the specified username
+        /// </summary>
+        /// <param name="channel">the channle to send the message to. if blank the message will be posted to the channel specified in the slack integration screen</param>
+        /// <param name="message">the message to send</param>
+        /// <param name="username">the username the message will apear to come from. if blank the username will be the username specified in the slack integration screen</param>
+        /// <returns>true if successful; otherwise false</returns>
+        public bool Send(string channel, string message, string username)
         {
-            return Send(channel, message, user, "");
+            return Send(channel, message, username, "");
         }
 
 
         /// <summary>
         /// send the message to slack, to the specified channel
         /// </summary>
-        /// <param name="channel">the channel to send the message to; if empty string it will default to #general</param>
+        /// <param name="channel">the channel to send the message to. if blank the message will be posted to the channel specified in the slack integration screen</param>
         /// <param name="message">the message to send</param>
+        /// <param name="username">the username the message will apear to come from. if blank the username will be the username specified in the slack integration screen</param>
+        /// <param name="avatar">the icon to use. can be a fully formed absolute URI, or an emoji identifier (eg ":alien:"). See slack integration screens for info on changing the icon.</param>
+        /// <exception cref="UriFormatException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         /// <returns>true if successful; otherwise false</returns>
-        public bool Send(string channel, string message, string user, string avatar)
+        public bool Send(string channel, string message, string username, string avatar)
         {
             bool result = false;
 
-            if (IsValidUri(config.WebHookURL) == false)  throw new UriFormatException("Invalid WebHookURL");
+            if (IsValidUri(config.WebHookURL, true) == false)  throw new UriFormatException("Invalid Slack WebHook URL");
             if (string.IsNullOrEmpty(message.Trim())) throw new ArgumentException("Message is empty");
 
             try
             {
                 string ch = string.IsNullOrEmpty(channel) ? config.Channel : channel;
-                string us = string.IsNullOrEmpty(user) ? config.Username : user;
+                string us = string.IsNullOrEmpty(username) ? config.Username : username;
                 string av = string.IsNullOrEmpty(avatar) ? config.Avatar : avatar;
 
                 StringBuilder sb = new StringBuilder();
@@ -97,7 +114,17 @@ namespace SlackNotify
                 sb.AppendFormat("\"text\": \"{0}\"", message);
                 if (string.IsNullOrEmpty(ch) == false) sb.AppendFormat(",\"channel\": \"{0}\"", ch);
                 if (string.IsNullOrEmpty(us) == false) sb.AppendFormat(",\"username\": \"{0}\"", us);
-                if (string.IsNullOrEmpty(av) == false) sb.AppendFormat(",\"icon_emoji\": \"{0}\"", av);
+                if (string.IsNullOrEmpty(av) == false)
+                {
+                    if (IsValidUri(av))
+                    {
+                        sb.AppendFormat(",\"icon_url\": \"{0}\"", av);
+                    }
+                    else
+                    {
+                        sb.AppendFormat(",\"icon_emoji\": \"{0}\"", av);
+                    }
+                }
                 sb.Append("}");
 
                 var client = new WebClient();
@@ -114,18 +141,60 @@ namespace SlackNotify
 
 
         /// <summary>
-        /// Checks for a valid URI & URI Scheme, which needs to be Https
+        /// Send a preformatted json payload to slack. Useful if you need to send message attachments as specified in the slack integration screens
+        /// </summary>
+        /// <param name="payload">the json payload to send</param>
+        /// <exception cref="UriFormatException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <returns>true if successful; otherwise false</returns>
+        public bool SendPreformattedJson(string payload)
+        {
+            bool result = false;
+
+            if (IsValidUri(config.WebHookURL, true) == false) throw new UriFormatException("Invalid Slack WebHook URL");
+            if (string.IsNullOrEmpty(payload)) throw new ArgumentException("Payload is empty");
+
+            try
+            {
+                var client = new WebClient();
+                string responsedata = client.UploadString(config.WebHookURL, payload);
+                result = responsedata.Equals("ok") ? true : false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error : " + e.Message);
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Checks for a valid URI and URI Scheme, which needs to be Https
         /// </summary>
         /// <param name="uriName">The Uri to test</param>
         /// <returns>true if valid; otherwise false</returns>
         bool IsValidUri(string uriName)
         {
+            return IsValidUri(uriName, false);
+        }
+
+
+        /// <summary>
+        /// Checks for a valid URI and URI Scheme
+        /// </summary>
+        /// <param name="uriName">The Uri to test</param>
+        /// <returns>true if valid; otherwise false</returns>
+        bool IsValidUri(string uriName, bool Https)
+        {
             Uri uriResult;
-            return Uri.TryCreate(uriName, UriKind.Absolute, out uriResult) && uriResult.Scheme == Uri.UriSchemeHttps;
+            bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult);
+            if (result && Https)
+            {
+                result = uriResult.Scheme == Uri.UriSchemeHttps;
+            }
+            return result;
         }
     }
-
-
-
-    
+  
 }
